4cb18525b5fec8e612d17c96d6309694c5e5f34c
Christian Koch <cfkoch@sdf.lonestar.org>
2016-01-23 20:26:55 -0800

    Include stdlib.h here, too, for alloca(3).

--- modules/gdscript/gd_script.cpp
+++ modules/gdscript/gd_script.cpp
@@ -26,6 +26,8 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
+#include <stdlib.h>
+
 #include "gd_script.h"
 #include "globals.h"
 #include "global_constants.h"

9cb98ecb169dbd9575e711e459fea974e0dbd11d
Christian Koch <cfkoch@sdf.lonestar.org>
2016-02-27 01:01:36 -0800

    More alloca(3) fixes.

--- core/math/triangle_mesh.cpp
+++ core/math/triangle_mesh.cpp
@@ -26,6 +26,7 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
+#include <stdlib.h>
 #include "triangle_mesh.h"
 #include "sort.h"
 
